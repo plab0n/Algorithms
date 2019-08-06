37a5222435648eda4a0ababd87424cee2b818b24
Plabon Biswas <plaboniubat16@gmail.com>
2019-08-07 01:29:00 +0600

    Update Segment_tree_lazy_with_propagation.cpp

--- Segment_tree_lazy_with_propagation.cpp
+++ Segment_tree_lazy_with_propagation.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 #define mx 100009
 typedef long long i64;
 using namespace std;
@@ -7,29 +9,15 @@ i64 arr[mx];
 int l,r;
 struct info{
     i64 sum,prop;
-}tree[mx*2];
+}tree[mx*3];
 
-void init(int node,int start,int end)
-{
-    if(start==end)
-    {
-        tree[node].sum = arr[start];
-    }
-    int mid = (start+end)/2;
-    int left = node*2;
-    int right = node*2+1;
-
-    init(left,start,mid);
-    init(right,mid+1,end);
-    tree[node].sum = tree[left].sum+tree[right].sum;
-}
 void update(int node,int start,int end,int val)
 {
     if(l>end || r<start)
         return ;
     if(start>=l && end<=r)
     {
-        tree[node].sum = val*(end-start+1);
+        tree[node].sum += val*(end-start+1);
         tree[node].prop += val;
         return ;
     }
@@ -51,34 +39,70 @@ i64 Query(int node,int start,int end,int carry =0)
     int mid = (start+end)/2;
     int left = node*2;
     int right = node*2+1;
-    i64 p1 = Query(left,start,mid,carry+tree[left].prop);
-    i64 p2 = Query(right,mid+1,end,carry+tree[right].prop);
+    i64 p1 = Query(left,start,mid,carry+tree[node].prop);
+    i64 p2 = Query(right,mid+1,end,carry+tree[node].prop);
     return p1+p2;
 }
-int main()
-{
-    int t,n,q;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    int c[100009];
+    int h[100009];
+    int cnt[100009];
+    int cntH[100009];
     cin>>t;
-    while(t--)
-    {
-        cin>>n>>q;
-        for(int i=1; i<=n*2; i++){
-            tree[i].sum = 0;
+
+    while(t--){
+        memset(cnt, 0, sizeof(cnt));
+        memset(cntH, 0, sizeof(cntH));
+        int n;
+        cin>>n;
+
+        for(int i=1; i<=n+n+n; i++){
+            tree[i].sum  = 0;
             tree[i].prop = 0;
         }
-            int type,v;
-        while(q--)
-        {
-            scanf("%d",&type);
-            if(type==0)
-            {
-               scanf("%d%d%d",&l,&r,&v);
-                update(1,1,n,v);
+
+        for(int i=1; i<=n; i++)cin>>c[i];
+        for(int i=1; i<=n; i++){
+            cin>>h[i];
+            if(h[i]>n)continue;
+            cntH[h[i]]++;
+        }
+
+        for(int i=1; i<=n; i++){
+            l = max(i-c[i], 1);
+            r = min(i+c[i], n);
+           // cout<<l<<" "<<r<<endl;
+            update(1, 1, n, 1);
+        }
+        for(int i=1; i<=n; i++){
+            l = i;
+            r = i;
+         //   cout<<Query(1,1,n,0)<<endl;
+            cnt[Query(1, 1, n, 0)]++;
+        }
+        int res = 0;
+        bool loc = true;
+        for(int i=1; i<=n; i++){
+            if(h[i]>n){
+                loc = false;
+                break;
             }
-            else{
-                scanf("%d%d",&l,&r);
-                printf("%lld\n",Query(1,1,n,0));
+            if(cnt[h[i]]!=cntH[h[i]]){
+                loc = false;
+                break;
             }
         }
+        //cout<<res<<endl;
+        if(loc){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
     }
+
+    return 0;
 }
